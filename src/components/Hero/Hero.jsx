934b46c4a8f5651cced70ec9cb9085ebc4288cc5
Yadhunath2003 <yadhunath2003@gmail.com>
2026-01-28 15:04:37 -0600

    edited hero page

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,13 +7,16 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm Yadhunath</h1>
-        <p className={styles.description}>
-          Computer Science major at the University of Kansas with a passion for 
+      <h1 className={`${styles.title} ${styles.reveal} ${styles.delay1}`}>
+        Hi, I'm Yadhunath
+      </h1>
+
+      <p className={`${styles.description} ${styles.reveal} ${styles.delay2}`}>
+      Computer Science major at the University of Kansas with a passion for 
           building innovative and scalable solutions. My experience spans full-stack development, data engineering, 
           and analytics, where I’ve worked on projects that combine backend systems, cloud platforms, and data pipelines 
           to deliver real-world impact.
-        </p>
+      </p>
         <a href="mailto:yadhunath2003@email.com" className={styles.contactBtn}>
           Contact Me
         </a>
